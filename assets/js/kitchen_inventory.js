40e3a36c2b39a17d0a486c3b834ab02c95e6105e
MKellman26 <57738618+MKellman26@users.noreply.github.com>
2021-04-05 20:53:48 -0500

    Monique: sent current username as post to ViewInventory.php

--- assets/js/kitchen_inventory.js
+++ assets/js/kitchen_inventory.js
@@ -2,14 +2,13 @@
 window.onload = function(){
     console.log("here")
 
-
+	console.log(sessionStorage.getItem('this_user'));
     let url = "./assets/php/ViewInventory.php";
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
   
       if(this.readyState === this.DONE && this.status === 200){
           
-  
         let users = this.responseText;
         console.log(users)
       document.getElementById("result-ingredients").innerHTML = users;
@@ -18,6 +17,6 @@ window.onload = function(){
     };
     xhttp.open("POST",url);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhttp.send();
+    xhttp.send("username=" + sessionStorage.getItem('this_user'));
     // }
   }
