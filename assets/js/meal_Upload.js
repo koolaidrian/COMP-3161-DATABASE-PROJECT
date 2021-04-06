05dbd096d427591fe45d5f6ad526799567722edd
Shazthestorylover <37606230+Shazthestorylover@users.noreply.github.com>
2021-04-06 17:43:35 -0500

    Currently working on Meal Upload

--- assets/js/meal_Upload.js
+++ assets/js/meal_Upload.js
@@ -1,42 +1,36 @@
-<<<<<<< Updated upstream
-=======
-/* Please note --> This file is still unfinished but still being worked on. */
+/* Meal Upload still being worked on... */
 
-/* 
->>>>>>> Stashed changes
-window.onload = function(){
+window.onload = function()
+{
 
     console.log("Meal upload starting...");
 
-    let url = "meal_upload.php";
-<<<<<<< Updated upstream
-    let username = document.getElementById('username').value;
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
-
-
-    /* 
-    function collectMealInfo()
-    {
-        //Getting all the data from the input fields and upload button
-        Array.from(document.querySelectorAll('#form-content input mealimage')).reduce((acc, input) => ({ ...acc, [input.id]: input.value}), {});
-    }
-
-    function collectRecipeIngredients()
+    let url = "meal_Upload.php";
+    
+    /* window.onload = function()
     {
         console.log("function under construction");
         //Array.from(document.querySelectorAll('#form-content-1 input')).reduce((acc, input) => ({ ...acc, [input.id]: input.value}), {});
-    } */
-}
-=======
-    let mealName = document.getElementById('mealName').value;
-    let mealType = document.getElementById('mealType').value;
-    let mealDesc = document.getElementById('mealDesc').value;
-    let mealImage = document.getElementById('mealImage').value;
+    }  */
 
-    let ingredientQuantity = document.getElementById('ingredients-qty').value;
-    let ingredientMeasurement = document.getElementById('ingredients-measurement').value;
-    let ingredientName = document.getElementById('ingredients-name').value;
+
+
+    let mealName = document.getElementById('mealName');
+    let mealType = document.getElementById('mealType'); // Select type --> How to select the chosen value from a list of options (i.e. need to research)
+    let mealDesc = document.getElementById('mealDesc');
+    let mealImage = document.getElementById('mealImage');
+
+    let ingredientQuantity = document.getElementById('ingredients-qty'); // Select type --> How to select the chosen value from a list of options (i.e. need to research)
+    let ingredientMeasurement = document.getElementById('ingredients-measurement'); // Select type --> How to select the chosen value from a list of options (i.e. need to research)
+    let ingredientName = document.getElementById('ingredients-name');
+
+    // Buttons to change screens
+    let prevButton = document.getElementsByClassName("btn btn btn-primary js-btn-prev");
+    let nextButton = document.getElementsByClassName("btn btn btn-primary js-btn-next");
+
+    //Adding event listeners for prv and next buttons --> Meant to change screens when prev and next buttons are clicked.
+    
+
 
 
 
@@ -60,7 +54,8 @@ window.onload = function(){
         function addIngredients(ingredientQuantity, ingredientMeasurement, ingredientName)
         {
             //Storing the quantity, measurement and name of an ingredient in an array.
-            //let lst_Of_Ingredients = [];
+            console.log("Add Ingredients under construction...");
+            let lst_Of_Ingredients = [];
 
         }
 
@@ -68,6 +63,7 @@ window.onload = function(){
         function removeIngredients()
         {
             //Removing a particular ingredient from the array.
+            console.log("Remove Ingredients under construction...");
             //let recipeInstructions = [];
         }
 
@@ -78,6 +74,7 @@ window.onload = function(){
     function collectRecipeInstructions()
     {
         console.log("Collect Recipe Instruction under construction...");
+
 
         //Adding an step for a recipe
         function addInstruction()
@@ -90,6 +87,7 @@ window.onload = function(){
         {
             console.log("Remove instruction currently being worked on...");
         }
+
 
     }
 
@@ -126,5 +124,4 @@ window.onload = function(){
 
 } 
 
-*/
->>>>>>> Stashed changes
+
