21f8a96c1b2a00e5fb2f4d77470920a92c6d8ffe
Shazthestorylover <37606230+Shazthestorylover@users.noreply.github.com>
2021-04-06 20:09:08 -0500

    Made some progress on meal upload

--- assets/js/meal_Upload.js
+++ assets/js/meal_Upload.js
@@ -29,7 +29,6 @@ window.onload = function()
     let nextButton = document.getElementsByClassName("btn btn btn-primary js-btn-next");
 
     //Adding event listeners for prv and next buttons --> Meant to change screens when prev and next buttons are clicked.
-    
 
 
 
@@ -88,6 +87,52 @@ window.onload = function()
             console.log("Remove instruction currently being worked on...");
         }
 
+
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+        // Upload file
+    function uploadFile() {
+
+        var files = document.getElementById("file").files;
+
+        if(files.length > 0 )
+        {
+
+            var formData = new FormData();
+            formData.append("file", files[0]);
+
+            var xhttp = new XMLHttpRequest();
+
+            // Set POST method and ajax file path
+            xhttp.open("POST", "ajaxfile.php", true);
+
+            // call on request changes state
+            xhttp.onreadystatechange = function() 
+            {
+                if (this.readyState == 4 && this.status == 200) 
+                {
+                    var response = this.responseText;
+                    if(response == 1)
+                    {
+                        alert("Upload successfully.");
+                    }
+                    else
+                    {
+                        alert("File not uploaded.");
+                    }
+                }
+            };
+
+            // Send request with data
+            xhttp.send(formData);
+
+        }
+        else
+        {
+            alert("Please select a file");
+        }
 
     }
 
